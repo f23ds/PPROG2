ec8c99ba7b7bacffd0a6c1bd915ae7449264fcbc
sylviarayn <99255511+sylviarayn@users.noreply.github.com>
2022-04-18 20:48:24 +0200

    Update p3_e1.c

--- P3/p3_e1.c
+++ P3/p3_e1.c
@@ -7,9 +7,9 @@
 #include <string.h>
 #include <limits.h>
 
-#define ct(X) (CONSTANTS + (X))
-#define TAM 4
 
+#define TAM 4
+#define ct(X) (CONSTANTS + (X))
 int CONSTANTS[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
                    10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
                    20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
